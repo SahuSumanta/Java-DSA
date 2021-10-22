4d44173f6aba298d120f594276b0fad9e549ecd5
SahuSumanta <smsahu9@gmail.com>
2021-10-22 18:46:33 +0530

    Java-DSA

--- Array/Medium/Set_Matrix_Zeroes.java
+++ Array/Medium/Set_Matrix_Zeroes.java
@@ -21,19 +21,25 @@ class Solution {
             }
         }
         
-        for(int i = 1; i < n; i++){
-            if(matrix[i][0] == 0){
-                for(int j = 0; j < m; j++){
-                    matrix[i][j] = 0;
-                }
-            }
-        }
+//         for(int i = 1; i < n; i++){
+//             if(matrix[i][0] == 0){
+//                 for(int j = 0; j < m; j++){
+//                     matrix[i][j] = 0;
+//                 }
+//             }
+//         }
         
-        for(int j = 1; j < m; j++){
-            if(matrix[0][j] == 0){
-                for(int i = 0; i < n; i++){
+//         for(int j = 1; j < m; j++){
+//             if(matrix[0][j] == 0){
+//                 for(int i = 0; i < n; i++){
+//                     matrix[i][j] = 0;
+//                 }
+//             }
+//         }
+       for(int i = 1; i < n; i++){
+            for(int j = 1; j < m; j++){
+                if(matrix[0][j] == 0 || matrix[i][0] == 0)
                     matrix[i][j] = 0;
-                }
             }
         }
         
