cf95ca46388da934a0d8d63b81c7bb5ee6efdf51
SahuSumanta <smsahu9@gmail.com>
2021-10-24 00:13:03 +0530

    Java-DSA

--- LinearSearch/Search_In_2DArrays.java
+++ LinearSearch/Search_In_2DArrays.java
@@ -1,3 +1,28 @@
+import java.util.Arrays;
+
 public class Search_In_2DArrays {
-    
+    public static void main(String[] args) {
+        int[][] arr = {
+                    {4,3,1},
+                    {9,3,2},
+                    {5,7,2}
+        };
+
+        int target = 9;
+        int[] ans = linersearch(arr,target); // format of return value {row, col}
+        System.out.println(Arrays.toString(ans));
+
+    }
+
+    static int[] linersearch(int[][] arr, int target) {
+
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                if (arr[row][col] == target) {
+                    return new int[]{row, col};
+                }
+            }
+        }
+        return new int[]{-1, -1};
+}
 }
