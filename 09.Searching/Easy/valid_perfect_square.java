a55c8d131fbb557ccf29a628fbbf6a9a256d0a78
SahuSumanta <smsahu9@gmail.com>
2021-10-29 00:29:39 +0530

    Java-DSA

---
+++ 09.Searching/Easy/valid_perfect_square.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        
+        if(num == 1){
+            return true;
+        }
+        
+        for(int i = 1; i < num; i++){
+            if(i*i == num){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+
+// Optimised code
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        
+        long left = 1, right = num;
+        
+        while (left <= right) {
+            
+            long mid = (left + right) / 2;
+            
+            if (mid * mid == num) return true; 
+            
+            if (mid * mid < num) {
+                
+                left = mid + 1;
+                
+            } else {
+                right = mid - 1; 
+            }
+        }
+        return false;
+    }
+}
