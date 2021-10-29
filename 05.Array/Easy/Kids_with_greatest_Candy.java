e6dfb203dea57aeca899fbbe61b481c0f9fe5e29
SahuSumanta <smsahu9@gmail.com>
2021-10-30 00:18:16 +0530

    Java-DSA

--- 05.Array/Easy/Kids_with_greatest_Candy.java
+++ 05.Array/Easy/Kids_with_greatest_Candy.java
@@ -1,3 +1,17 @@
+/*
+
+Input: candies = [2,3,5,1,3], extraCandies = 3
+Output: [true,true,true,false,true] 
+Explanation: If you give all extraCandies to:
+- Kid 1, they will have 2 + 3 = 5 candies, which is the greatest among the kids.
+- Kid 2, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
+- Kid 3, they will have 5 + 3 = 8 candies, which is the greatest among the kids.
+- Kid 4, they will have 1 + 3 = 4 candies, which is not the greatest among the kids.
+- Kid 5, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
+
+
+*/
+
 class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
         
