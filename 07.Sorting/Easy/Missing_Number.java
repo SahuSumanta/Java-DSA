70410602766dbf60326ec994a184722a0af45674
SahuSumanta <smsahu9@gmail.com>
2021-11-17 20:44:39 +0530

    Missing Number

--- 07.Sorting/Easy/Missing_Number.java
+++ 07.Sorting/Easy/Missing_Number.java
@@ -10,7 +10,18 @@ public class Missing_Number {
             return nums.length;
         }
 
+        //optimised one but not using sorting
         
+        // public int missingNumber(int[] nums) {
+        //     int sum=0;
+        //     for(int i=0;i<=nums.length;i++)
+        //         sum+=i;
+    
+        //     for(int i=0;i<nums.length;i++)
+        //         sum-=nums[i];
+    
+        //     return sum;
+        // }
 
     
 }
