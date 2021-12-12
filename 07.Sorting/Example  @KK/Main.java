62646c67716f223e4faa017a25d67c757c885218
SahuSumanta <smsahu9@gmail.com>
2021-12-12 08:18:40 +0530

    Sorting Algo code

--- 07.Sorting/Example  @KK/Main.java
+++ 07.Sorting/Example  @KK/Main.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class Main{
     public static void main(String[] args) {
         int[] arr = {5,3,4,1,2};
-        bubble(arr);
+        bubblesort(arr);
         selection(arr);
         instertion(arr);
         cyclicsort(arr);
@@ -48,24 +48,19 @@ public class Main{
         }
     }
 
-    private static void swap(int[] arr, int maxIndex, int lastIndex) {
-        int temp = arr[maxIndex];
-        arr[maxIndex] = arr[lastIndex];
-        arr[lastIndex] = temp;
-    }
-
     static int getMax(int[] arr, int start, int end){
-        int max = start;
-        for(int i = start; i <= end; i++){
-            if(arr[max] < arr[i]){
-                max = i;
+            int max = start;
+            for(int i = start; i <= end; i++){
+                if(arr[max] < arr[i]){
+                    max = i;
+                }
             }
+            return max;
         }
-        return max;
-    }
+
 
     //Bubble sort
-    static void bubble(int[] arr){
+    static void bubblesort(int[] arr){
         for (int i = 0; i < arr.length; i++) {
             for (int j = 1; j < arr.length; j++) {
                 if(arr[j] < arr[j-1]){
@@ -78,5 +73,11 @@ public class Main{
         }
 
 
+    }
+
+    private static void swap(int[] arr, int maxIndex, int lastIndex) {
+        int temp = arr[maxIndex];
+        arr[maxIndex] = arr[lastIndex];
+        arr[lastIndex] = temp;
     }
 }
