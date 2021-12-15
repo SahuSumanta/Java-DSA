8df4f8cb830a6c7bf9c2693c20c555bb85aafaa3
SahuSumanta <smsahu9@gmail.com>
2021-12-15 11:58:00 +0530

    Search Eliment LinearSearch

--- 10.Recursion/Easy/Findeliment.java
+++ 10.Recursion/Easy/Findeliment.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+ 
+
 public class Findeliment {
     
-
+    //Method-1
     //return index of the terget eliment
     static int findindex(int[] arr, int target, int index){
 
@@ -16,7 +19,32 @@ public class Findeliment {
 
         
     }
+    //Method - 2
+    //find all index
+    static ArrayList<Integer> list = new ArrayList<>();
+    static void findAllIndex(int[] arr, int target, int index) {
+        if (index == arr.length) {
+            return;
+        }
+        if (arr[index] == target) {
+            list.add(index);
+        }
+        findAllIndex(arr, target, index + 1);
+    }
 
+    //Method-3
+    //Here arraylist is used as a argument inside the dunction findall index
+    static ArrayList<Integer> findAllIndex(int[] arr, int target, int index, ArrayList<Integer> list) {
+        if (index == arr.length) {
+            return list;
+        }
+        if (arr[index] == target) {
+            list.add(index);
+        }
+        return findAllIndex(arr, target, index + 1, list);
+    }
+
+    //Method-4
     //return true or false
     static boolean search(int[] arr, int target, int index){
 
@@ -29,9 +57,13 @@ public class Findeliment {
     }
 
     public static void main(String[] args) {
-        int[] arr = {3,2,1,18,19};
-        int target = 18;
-        System.out.println(findindex(arr, target, 0));
-        System.out.println(search(arr, target, 0));
+        int[] arr = {2,3,1,4,4,5};
+        int target = 4;
+        System.out.println(findindex(arr, target, 0)); // find index
+        System.out.println(search(arr, target, 0)); // return true or false
+        //find multiple index
+        findAllIndex(arr, target, 0);
+        System.out.println(list);
+        System.out.println(findAllIndex(arr, target, 0, list));
     }
 }
