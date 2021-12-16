2b5abc01f865181bf7c867fdbfd3bc85af3e3e78
SahuSumanta <smsahu9@gmail.com>
2021-12-16 12:25:50 +0530

    Pattern Questions  using Recursion

--- 09.Patterns/Recursion_Pattern.java
+++ 09.Patterns/Recursion_Pattern.java
@@ -1,21 +1,37 @@
 public class Recursion_Pattern {
     
 
-    static void triangle(int row, int col){
+    static void triangle1(int row, int col){
         if(row == 0){
             return;
         }
 
         if(col < row){
             System.out.print("*");
-            triangle(row, col+1);
+            triangle1(row, col+1);
         }
         else{
             System.out.println(); //newline
-            triangle(row-1, 0);
+            triangle1(row-1, 0);
+        }
+    }
+
+    static void triangle2(int row, int col){
+        if(row == 0){
+            return;
+        }
+
+        if(col < row){
+            triangle2(row, col+1);
+            System.out.print("*");
+        }
+        else{
+            triangle2(row-1, 0);
+            System.out.println(); //newline
         }
     }
     public static void main(String[] args) {
-        triangle(4, 0);
+        triangle1(4, 0);
+        triangle2(4, 0);
     }
 }
