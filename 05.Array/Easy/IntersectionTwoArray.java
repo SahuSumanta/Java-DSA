51ca9f124d0afefd0e8a8084f0fd8c0fb5b55d63
SahuSumanta <smsahu9@gmail.com>
2022-01-22 08:20:37 +0530

    Intersection two array using two pointer

--- 05.Array/Easy/IntersectionTwoArray.java
+++ 05.Array/Easy/IntersectionTwoArray.java
@@ -33,3 +33,15 @@ public class IntersectionTwoArray {
             System.out.print(op+" ");
     }
 }
+
+
+/*abstractApproach 2 (Using Two Pointers)
+If the elements of both the array is sorted then this approach is more efficient. For this problem as it is not sorted we sort both the arrays first.
+Now we will use two pointers i and j for the two arrays and initialize both with zero.
+Move index i along 1st array(nums1) and index j along 2nd array(nums2)
+
+Compare the two elements pointed by i and j.
+If nums1[i] is less than nums2[j] then we know that we have to leave the smaller element and go to next(greater) element in nums1 array so as to check for intersection of elements.
+Else if nums1[i] is greater than nums2[j] then similarly we have to go to next(greater) element in nums2 array.
+Else both the elements intersected, hence add this element to result array. And Increment both i and j.
+*/
