224613e4aa83e836f1613d827ec9b04cfd70e71c
SahuSumanta <smsahu9@gmail.com>
2022-02-02 22:47:24 +0530

    Math Example Added  @KK

---
+++ 12.Math/Example/NewtonSQRT.java
@@ -0,0 +1,19 @@
+public class NewtonSQRT {
+    public static void main(String[] args) {
+        System.out.println(sqrt(40));
+    }
+    static double sqrt(double n) {
+        double x = n;
+        double root;
+        while (true) {
+            root = 0.5 * (x + (n/x));
+
+            if (Math.abs(root - x) < 0.5) {
+                break;
+            }
+
+            x = root;
+        }
+        return root;
+    }
+}
